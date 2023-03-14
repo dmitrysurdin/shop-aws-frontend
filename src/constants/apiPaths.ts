b352a3608531e9291cbc36f802fa2a032df0bc19
Dmitry Surdin <dimasurdin@yandex.ru>
2023-03-14 14:52:37 +0400

    refactoring

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://yn3i62tbcf.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://480xxj9rj3.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://yn3i62tbcf.execute-api.eu-west-1.amazonaws.com/dev",
-  getById: "https://480xxj9rj3.execute-api.eu-west-1.amazonaws.com/dev",
+  create: " https://zrka0tni55.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
