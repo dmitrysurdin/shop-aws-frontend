c079fd98258b1efc829323f1eccc695264c0d38f
Dmitry Surdin <dimasurdin@yandex.ru>
2023-03-14 14:31:58 +0400

    task-4

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,6 +3,7 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://yn3i62tbcf.execute-api.eu-west-1.amazonaws.com/dev",
+  getById: "https://480xxj9rj3.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
--- src/queries/products.ts
+++ src/queries/products.ts
@@ -26,7 +26,7 @@ export function useAvailableProduct(id?: string) {
     ["product", { id }],
     async () => {
       const res = await axios.get<AvailableProduct>(
-        `${API_PATHS.bff}/product/${id}`
+        `${API_PATHS.getById}/product/${id}`
       );
       return res.data;
     },
@@ -45,7 +45,7 @@ export function useRemoveProductCache() {
 
 export function useUpsertAvailableProduct() {
   return useMutation((values: AvailableProduct) =>
-    axios.put<AvailableProduct>(`${API_PATHS.bff}/product`, values, {
+    axios.put<AvailableProduct>(`${API_PATHS.bff}/products`, values, {
       headers: {
         Authorization: `Basic ${localStorage.getItem("authorization_token")}`,
       },
